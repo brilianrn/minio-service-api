2f3371e2024c26cb09c1964db7c4c1ac99d07db9
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2023-06-14 19:41:29 +0700

    change env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config({
+  path: `.env-${process.env.NODE_ENV || "development"}`,
+});
 
 const express = require("express");
 const app = express();
